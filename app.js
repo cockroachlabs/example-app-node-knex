142ebc1a245617df49db4ded9a7320947969f605
Rafi Shamim <rafi@cockroachlabs.com>
2024-12-02 13:13:22 -0500

    Add txn retry wrapper to init step

--- app.js
+++ app.js
@@ -131,7 +131,7 @@ async function deleteAccounts(client, transaction) {
 (async () => {
   // Initialize table in transaction retry wrapper
   console.log("Initializing accounts table...");
-  await initTable(client);
+  await retryTxn(0, 15, client, initTable);
 
   // Transfer funds in transaction retry wrapper
   console.log("Transferring funds...");
